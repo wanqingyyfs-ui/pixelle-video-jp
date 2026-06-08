4360531c031c52221ac0e5ae5b62f1e8e485570e
万能青年 <wanqingyyfs@gmail.com>
2026-06-08 09:48:31 +0700

    Remove runtime environment file from repository

--- .runtime_env.ps1
+++
@@ -1,3 +0,0 @@
-$env:OPENAI_API_KEY = "sk-5NmvPwGBGSp425Lj5QicQ2AedaPCPWJIruVjtlu3QgsI6ES7"
-$env:OPENAI_ADMIN_KEY = "sk-5NmvPwGBGSp425Lj5QicQ2AedaPCPWJIruVjtlu3QgsI6ES7"
-$env:OPENAI_BASE_URL = "https://api.nuwaflux.com/v1"
